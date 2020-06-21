4a9e1af5dbc8d2fff98b950e44b7362811abe63f
ChandraSekharReddy <tchandu.soft@gmail.com>
2020-06-21 18:35:14 +0530

    Update: Code changes

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -13,7 +13,93 @@ export class SidebarComponent implements OnInit {
   ngOnInit(): void {
     this.items = [
       {
+        label: 'Dashboard',
+        icon: 'fa fa-credit-card',
+        expanded: true,
+        routerLink: '/dashboard',
+        visible: true,
+      },
+      {
 
+        label: 'Payments',
+        icon: 'fa fa-credit-card',
+        routerLink: '/payments',
+        expanded: false,
+      }
+      ,
+      {
+        label: 'Reports',
+        icon: 'fa fa-file-text-o',
+        routerLink: '/reports',
+        expanded: false,
+        visible: true,
+      },
+      {
+        label: 'Global Settings',
+        icon: 'fa fa-cog',
+        routerLink: '/global-settings',
+        expanded: false,
+        visible: true,
+      },
+      {
+        label: 'Dashboard',
+        icon: 'fa fa-credit-card',
+        expanded: true,
+        routerLink: '/dashboard',
+        visible: true,
+      },
+      {
+
+        label: 'Payments',
+        icon: 'fa fa-credit-card',
+        routerLink: '/payments',
+        expanded: false,
+      }
+      ,
+      {
+        label: 'Reports',
+        icon: 'fa fa-file-text-o',
+        routerLink: '/reports',
+        expanded: false,
+        visible: true,
+      },
+      {
+        label: 'Global Settings',
+        icon: 'fa fa-cog',
+        routerLink: '/global-settings',
+        expanded: false,
+        visible: true,
+      },
+      {
+        label: 'Dashboard',
+        icon: 'fa fa-credit-card',
+        expanded: true,
+        routerLink: '/dashboard',
+        visible: true,
+      },
+      {
+
+        label: 'Payments',
+        icon: 'fa fa-credit-card',
+        routerLink: '/payments',
+        expanded: false,
+      }
+      ,
+      {
+        label: 'Reports',
+        icon: 'fa fa-file-text-o',
+        routerLink: '/reports',
+        expanded: false,
+        visible: true,
+      },
+      {
+        label: 'Global Settings',
+        icon: 'fa fa-cog',
+        routerLink: '/global-settings',
+        expanded: false,
+        visible: true,
+      },
+      {
         label: 'Dashboard',
         icon: 'fa fa-credit-card',
         expanded: true,
